b35e58dc3c2ab4d4a8383123f4a8e1b29bcfd498
xmasui <arthur.matugi@gmail.com>
2020-08-24 20:52:38 +0000

    24/08 starting back-end project with Express

---
+++ back-end/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//criação de uma nova rota
+const teste = require('./routes/teste')
+app.use('/teste', teste) //comecar a validar teste passando a variavel
+
+module.exports = app;
